99945a1f608ffb4967ead7d09c58858622fe27c5
SusanZheng15 <zheng.susan12@gmail.com>
2016-03-06 03:46:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  int = 10
+  while int > 0
+    $stdout.puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  def countdown_with_sleep(sleep)
+    sleep(5)
+  end
+  return "HAPPY NEW YEAR!"
 end
